4855e237e1b1947f0673a1dee59ecdb8ddf300d7
Winterman981 <teerathanomnuea@gmail.com>
2021-11-26 20:09:02 +0700

    Bug Fix

--- Assets/Scripts/SubmitManager.cs
+++ Assets/Scripts/SubmitManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SubmitManager : MonoBehaviour
 {
@@ -15,7 +16,15 @@ public class SubmitManager : MonoBehaviour
         }
     }
 
-    [Header("Day 1 Bools")]
+	private void Update()
+	{
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+	[Header("Day 1 Bools")]
 
     public bool submitBakerPast;
     public bool submitUPoster;
